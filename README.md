9080c8e7f565f5826eee0c2590652bd016d2da29
Jukov Alexey <alexismaster@yandex.ru>
2015-08-26 10:22:55 +0300

    Первый коммит

---
+++ README.md
@@ -0,0 +1,25 @@
+
+
+# Установка OpenCV (Linux Debian)
+
+```
+sudo apt-get install libopencv-dev python-opencv
+```
+
+
+# Установка звуковой библиотеки (Linux Debian)
+
+```
+sudo apt-get install python-setuptools
+https://bitbucket.org/pyglet/pyglet/downloads/pyglet-1.2.3.tar.gz
+sudo python setup.py install
+```
+
+
+# Полезные статьи:
+
+```
+http://robot-develop.org/archives/2328
+http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_imgproc/py_morphological_ops/py_morphological_ops.html
+http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_imgproc/py_filtering/py_filtering.html
+```
---
+++ app.py
@@ -0,0 +1,110 @@
+# 
+# -*- coding: utf-8 -*-
+# 
+
+import numpy as np
+import cv2
+import pyglet
+
+
+countLimit  = 15                # Порог срабатывания тревоги (число обнаруженных белых пикселов)
+width       = 120               # Катет квадрата слежения
+point1      = (230, 245)        # Положение левого верхнего угла области слежения
+winLeft     = 1930              # Смещение окон с видео
+
+
+
+point2 = (point1[0] + width, point1[1] + width)
+cap    = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture('/home/alexismaster/Видео/video.avi')
+
+# начальные значения...
+t_minus = cv2.cvtColor(cap.read()[1], cv2.COLOR_RGB2GRAY)
+t       = cv2.cvtColor(cap.read()[1], cv2.COLOR_RGB2GRAY)
+t_plus  = cv2.cvtColor(cap.read()[1], cv2.COLOR_RGB2GRAY)
+
+
+# Окна для отображения видео с камеры
+winName1 = "Движущиеся объекты"
+winName2 = "Исходное видео"
+cv2.namedWindow(winName1, cv2.CV_WINDOW_AUTOSIZE)
+cv2.namedWindow(winName2, cv2.CV_WINDOW_AUTOSIZE)
+
+# Позиционирование окон
+cv2.moveWindow(winName2, 0 + winLeft, 0)
+cv2.moveWindow(winName1, 645 + winLeft, 0)
+
+
+isSignal   = 0
+intervalId = 50
+
+
+# Воспроизводит звуковой сигнал
+def buzzer():
+    global isSignal
+    
+    if isSignal == 0:
+        #song = pyglet.media.load('/home/alexismaster/Музыка/buzzer3_x.wav')
+        song = pyglet.media.load('./alarm.wav')
+        song.play()
+        isSignal = 100
+    return
+
+
+
+while(cap.isOpened()):
+    frame   = cap.read()
+    t_minus = t
+    t       = t_plus
+    t_plus  = cv2.cvtColor(frame[1], cv2.COLOR_RGB2GRAY)
+
+    if isSignal > 0:
+        isSignal = isSignal - 1
+    
+    # Из сглаженного текущего кадра (t_plus) вычитаем предыдущий (t_minus).
+    #t_minus = cv2.GaussianBlur(t_minus, (5,5), 0)
+    t_plus  = cv2.GaussianBlur(t_plus, (5,5), 0)
+    mask    = cv2.absdiff(t_minus, t_plus);
+
+    # Сравниваем значения полученной разности с некоторым пороговым значением
+    retval, mask = cv2.threshold(mask, 25, 255, cv2.THRESH_BINARY)
+
+    # Применяем морфологические операции закрытия и открытия, чтобы избавиться от 
+    # движущихся регионов малого размера (шумы камеры)
+    kernel          = np.ones((5,5), np.uint8)
+    mask            = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    mask            = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    mask_min        = cv2.getRectSubPix(mask, (width, width), (point1[0] + width/2, point1[1] + width/2))
+    countNonZero    = cv2.countNonZero(mask_min)
+
+    if intervalId > 0:
+        intervalId = intervalId - 1
+    else:
+        intervalId = 1000
+        print 'countNonZero = ', countNonZero
+
+    # Если обнаружено движение
+    if countNonZero > countLimit:
+        print 'motion detected, countNonZero = ', countNonZero
+        buzzer()
+
+    # Картинка 2
+    cv2.rectangle(mask, point1, point2, (255,0,0), 2)
+    cv2.imshow(winName1, mask)
+    
+    # Картинка 1
+    cv2.rectangle(frame[1], point1, point2, (0,153,204), 2, cv2.CV_AA)
+    cv2.imshow(winName2, frame[1])
+
+    # Маленькая картинка
+    #cv2.imshow('diff', im)
+
+    # Большие значения задержки позволяют лучше реагировать на медленное движение
+    # Для увеличения плавности видео задержку следует уменьшить (оптимальное значение - 25 ms)
+    if cv2.waitKey(50) & 0xFF == ord('q'):
+        break
+
+
+
+cap.release()
+cv2.destroyAllWindows()
