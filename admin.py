42d9d519da52cc95a0f29dfdd14a113dcd88495b
Jukov Alexey <alexismaster@yandex.ru>
2016-05-13 12:07:07 +0300

    Картинка с текущим кадром

--- admin.py
+++ admin.py
@@ -7,11 +7,32 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 
 class HttpProcessor(BaseHTTPRequestHandler):
+    
     def do_GET(self):
-        global point1, point2
-
         txt = open('admin/index.html')
 
+        self.moveRectangle()
+        saveCurrentImage()
+
+        # Картинка с текущим кадром
+        if self.path == "/screen.png":
+            self.send_response(200)
+            self.send_header("content-type", "image/png")
+            self.end_headers()
+            png = open('current.png')
+            self.wfile.write(png.read())
+            return
+
+        self.send_response(200)
+        self.send_header("content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(txt.read())
+        return
+
+    # Позиционирование области слежения
+    def moveRectangle(self) :
+        global point1, point2, width
+        
         # right
         if self.path == "/right":
             point1 = (point1[0] + 10, point1[1])
@@ -32,30 +53,20 @@ class HttpProcessor(BaseHTTPRequestHandler):
             point1 = (point1[0], point1[1] + 10)
             point2 = (point2[0], point2[1] + 10)
 
-        saveCurrentImage()
+        # width_plus
+        if self.path == "/width_plus":
+            width = width + 10
+            point2 = (point1[0] + width, point1[1] + width)
 
-        # Картинка с текущим кадром
-        if self.path == "/screen.png":
-            self.send_response(200)
-            self.send_header("content-type", "image/png")
-            self.end_headers()
-            png = open('current.png')
-            self.wfile.write(png.read())
-            return
+        # width_minus
+        if self.path == "/width_minus":
+            width = width - 10
+            point2 = (point1[0] + width, point1[1] + width)
 
-        self.send_response(200)
-        self.send_header("content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(txt.read())
+        saveSettings()
+        
+        return
 
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header("content-type", "text/html")
-        self.end_headers()
-        self.wfile.write("hello !")
-
-
-print 'admin.py'
 
 
 
